9aebcbc3f5f03e559c7daef650e7512b600be385
LorisPalmucci <generalle913@gmail.com>
2017-08-24 20:52:50 +0200

    implementazione codice database

--- src/GestioneMagazzino/AggiungiPezzo.java
+++ src/GestioneMagazzino/AggiungiPezzo.java
@@ -1,24 +1,147 @@
 package GestioneMagazzino;
+
+import java.util.Date;
 import java.util.Scanner;
 import GestioneMagazzino.Pezzo;
+import java.sql.*;
 
 public class AggiungiPezzo 
 {
-	public AggiungiPezzo()
+	private Connection connect = null;
+    private Statement statement = null;
+    private PreparedStatement preparedStatement = null;
+    private ResultSet resultSet = null;
+    
+	public void writeDatabase() throws Exception
 	{
-		do {
-			Pezzo prodotto=new Pezzo();
-			Scanner in=new Scanner(System.in);
+		Pezzo prodotto = new Pezzo();
+		Scanner in = new Scanner(System.in);	
+				
+		System.out.println("Inserisci in ordine ID, Prezzo, Quantità in Magazzino");
+		prodotto.setIdPezzo(in.nextLine());
+		prodotto.setPrezzoInEuro(in.nextInt());
+		prodotto.setQuantitàMagazzino(in.nextInt());
 		
-			System.out.println("Inserisci in ordine ID, Prezzo, Quantità in Magazzino");
-		
-			prodotto.setIdPezzo(in.nextLine());
-			prodotto.setPrezzoInEuro(in.nextInt());
-			prodotto.setQuantitàMagazzino(in.nextInt());
-			System.out.println("ID pezzo: "+prodotto.getIdPezzo() +'\n'+
-								"Prezzo: "+prodotto.getPrezzoInEuro() +'\n'+
-								"Quantità in Magazzino: "+prodotto.getQuantitàMagazzino());
+		try {
+            // Carico i driver di SQL
+            Class.forName("com.mysql.jdbc.Driver");
+            
+            // Imposto la connessione col DB
+            connect = DriverManager
+                    .getConnection("jdbc:mysql://localhost/magazzino","loris","password");
+
+            // Statements permette di usare le query di SQL con il DB
+            statement = connect.createStatement();
+            
+            // Preparo una stringa con la query pe il DBMS e la invia
+            String queryString="INSERT INTO magazzino.Pezzi(ID,Prezzo,QuantitàMagazzino) VALUES (" 
+            		+ "'" + prodotto.getIdPezzo() + "', " 
+            		+ prodotto.getPrezzoInEuro() + ", " 
+            		+ prodotto.getQuantitàMagazzino() + ");";
+            preparedStatement = connect.prepareStatement(queryString);
+            preparedStatement.executeUpdate();
 			}
-			while(true);
+		
+		catch (Exception e) 
+		{
+            throw e;
+        } 
+		finally 
+		{
+            close();
+        }
+	}
+	
+	public void readRecord() throws Exception
+	{	
+		String id;
+		Scanner inID = new Scanner(System.in);
+		System.out.println("Inserisci l'ID del pezzo che vuoi cercare: " + '\n');
+		
+		id = inID.nextLine();
+		
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			connect = DriverManager.getConnection("jdbc:mysql://localhost/magazzino","loris","password");
+			statement = connect.createStatement();
+			preparedStatement = connect.prepareStatement("SELECT * FROM magazzino.Pezzi WHERE ID = " + "'" + id + "';");
+			resultSet = preparedStatement.executeQuery();
+			writeResultSet(resultSet);											 
+		}
+		
+		catch(Exception e)
+		{
+			throw e;
+		}
+		
+		finally
+		{
+			close();
+		}
+	}
+	public void readDatabase() throws Exception
+	{
+		try
+		{
+            Class.forName("com.mysql.jdbc.Driver");
+            connect = DriverManager.getConnection("jdbc:mysql://localhost/magazzino","loris","password");
+            statement = connect.createStatement();
+			preparedStatement = connect.prepareStatement("SELECT ID, Prezzo, QuantitàMagazzino from magazzino.Pezzi");
+			resultSet = preparedStatement.executeQuery();
+			writeResultSet(resultSet);
+		}
+		
+		catch (Exception e) 
+		{
+            throw e;
+        } 
+		finally 
+		{
+            close();
+        }
+		
+	}
+	
+	private void writeResultSet(ResultSet resultSet) throws SQLException 
+	{
+		// ResultSet è inizializzato prima del primo dataset
+	    while (resultSet.next()) 
+	    {
+	    	String ID = resultSet.getString("ID");
+	    	String prezzo = resultSet.getString("Prezzo");
+	    	String qta = resultSet.getString("QuantitàMagazzino");
+	            
+	    	System.out.println("ID: " + ID);
+	    	System.out.println("Prezzo: " + prezzo);
+	    	System.out.println("Qtà: " + qta + '\n');        
+	    }
+	 }
+
+	// devo chiudere il resultSet
+	private void close() 
+	{
+
+		try 
+		{
+			if (resultSet != null) 
+            	{
+            		resultSet.close();
+            	}
+
+            if (statement != null)
+            	{
+                	statement.close();
+            	}
+
+            if (connect != null) 
+            	{
+            		connect.close();
+            	}
+        } 
+		catch (Exception e) 
+		{
+			
+        }
 	}
 }
--- src/Run/mainRun.java
+++ src/Run/mainRun.java
@@ -1,13 +1,39 @@
 package Run;
+
 import java.util.Scanner;
-
 import GestioneMagazzino.AggiungiPezzo;
+import GestioneMagazzino.MySqlAcces;
 
-public class mainRun {
-
-	public static void main(String[] args) 
+public class mainRun 
+{
+	static String opzione;
+	public static void main(String[] args) throws Exception 
 	{	
-		AggiungiPezzo aggiungi=new AggiungiPezzo();		
+		AggiungiPezzo aggiungi = new AggiungiPezzo();
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Digita il comando di cosa vuoi fare: " + '\n' 
+						   + "1.--> add:    Aggiungi un pezzo;" + '\n'
+						   + "2.--> show:   Visualizza il Database;" + '\n'
+						   + "3.--> search: Cerca uno specifico articolo;" + '\n');
+		
+		opzione = in.nextLine();		
+		if (opzione.equals("add"))
+			{
+				aggiungi.writeDatabase();
+			}
+		
+		else if (opzione.equals("show"))
+			{
+				aggiungi.readDatabase();
+			}
+		else if (opzione.equals("search"))
+			{
+				aggiungi.readRecord();
+			}
 	}
-
+		
+		
 }
+
+
